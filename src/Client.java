fc9590cd632e357fa6bf71c747de3b255a92e320
pleluron <pierre.leluron@gmail.com>
2017-11-21 09:42:16 +0100

    comments

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,15 @@
 
 public class Client implements Runnable {
+	// Identifiant du client
 	int numero;
+	// Reference vers la piscine
 	Piscine piscine;
+	
+	/**
+	 * Crée un client
+	 * @param piscine référence vers la piscine
+	 * @param numero numéro unique du client
+	 */
 	public Client(Piscine piscine, int numero) {
 		this.piscine = piscine;
 		this.numero = numero;
@@ -10,19 +18,25 @@ public class Client implements Runnable {
 	public void run() {
 		System.out.println("Le client " + numero + " est arrivé");
 		
+		// Achat du ticket au guichet
 		piscine.getPointDeVente().vendre(numero);
 		
+		// Mettage de maillot
 		piscine.getVestiaire().entrer(numero);
 		piscine.getVestiaire().seChanger(numero, true);
 		piscine.getVestiaire().sortir(numero);
 		
+		// Plongeage dans le bassin
 		piscine.getBassin().entrer(numero);
 		piscine.getBassin().nage(numero);
 		piscine.getBassin().sortir(numero);
 		
+		// Demettage de maillot
 		piscine.getVestiaire().entrer(numero);
 		piscine.getVestiaire().seChanger(numero, false);
 		piscine.getVestiaire().sortir(numero);
+		
+		// Sortissage de la piscine
 		System.out.println("Le client " + numero + " est parti");
 		return;
 	}
--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,14 @@ public class Main {
 	public static void main(String args[]) {
 		Piscine piscine = new Piscine();
 		ArrayList<Thread> clientThreadArray = new ArrayList<Thread>();
+		// Crée et lance un thread par client
 		for (int i=0; i <nbClients; i++) {
 			Thread thread = new Thread(new Client(piscine, i));
-			//thread.setDaemon(true);
 			thread.start();
 			clientThreadArray.add(thread);
 		}
 		
+		// Attend la fin des threads
 		for (Thread thread : clientThreadArray) {
 			try {
 				thread.join();
