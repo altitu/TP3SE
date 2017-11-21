905e791cd1df4ffa902a3cea75c0c287574cdf31
pleluron <pierre.leluron@gmail.com>
2017-11-21 10:01:04 +0100

    debut part2

--- src/part2/PointDeVente.java
+++ src/part2/PointDeVente.java
@@ -2,7 +2,22 @@ package part2;
 
 public class PointDeVente {
 	
-	private int temps = 5;
+	private final int temps = 5;
+	private boolean[] guichets = new boolean[2];
+	
+	public synchronized void allerAuGuichet(int num) {
+		if (guichets[0] && guichets[1]) {
+			System.out.println("Le client " + num + " attend car les " + guichets + " sont occupés");
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		guichetsOccupes++;
+		System.out.println("Le client " + num + " va au guichet");
+	}
 	
 	public synchronized void vendre(int num) {
 		try {
